de76b14385bd267e7c265d67c4e2e57def82956b
Bartosz Gruca <96384251+hisoroo@users.noreply.github.com>
2026-03-14 11:14:11 +0100

    feat(navbar): Add basic routing and navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,18 +4,20 @@ import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
+import { useColorScheme } from "nativewind";
 
 import { DARK_THEME, LIGHT_THEME } from "@/theme";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { colorScheme } = useColorScheme();
   const theme = colorScheme === "dark" ? DARK_THEME : LIGHT_THEME;
 
   return (
     <ThemeProvider value={theme}>
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
       <PortalHost />
     </ThemeProvider>
   );
