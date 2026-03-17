157439f25b6507743f33cbbe2d8994c9a503b2e1
Bartosz Gruca <96384251+hisoroo@users.noreply.github.com>
2026-03-17 15:54:54 +0100

    feat(discover): Add gym cards and scroll section

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,26 @@
 import "../global.css";
 
-import { ThemeProvider } from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "nativewind";
 
-import { DARK_THEME, LIGHT_THEME } from "@/theme";
+import { QueryProvider } from "@/src/providers/query-provider";
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
-  const theme = colorScheme === "dark" ? DARK_THEME : LIGHT_THEME;
+  const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return (
-    <ThemeProvider value={theme}>
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-      <PortalHost />
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider value={theme}>
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <PortalHost />
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
