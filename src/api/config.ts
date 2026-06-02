e7a0f02ef3066c6607b2c88e526f055b67f79db1
xmavv <xxmatheooo@gmail.com>
2026-06-02 02:08:02 +0200

    feat: add proper api url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "https://52aa-93-159-49-233.ngrok-free.app/v1";
+const DEFAULT_API_BASE_URL = "https://climbit.manamo101.tech/v1";
 
 export const API_BASE_URL = (process.env.EXPO_PUBLIC_API_BASE_URL ?? DEFAULT_API_BASE_URL).replace(
   /\/$/,
